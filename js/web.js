113296fc197907caaacd3b725b3e86f2c62432f5
tkdgur4273 <tkdgur4273@gmail.com>
2022-03-19 12:08:15 +0900

    변수와 데이터 타입

---
+++ js/web.js
@@ -0,0 +1,14 @@
+var count = 100;
+count += 50;
+count += "원";
+count += 50;
+count = 100;
+var count = "100원";
+var arr =["회사소개","제품소개","고객센터"]
+
+//number숫자, string문자, null, undefined, object
+
+console.log("count 변수값은 ", count,"데이터타입", typeof count );
+console.log("count 변수값은 ", arr[0],"데이터타입", typeof arr );
+
+// countr 변수값은? ... go 값은 고릴라
